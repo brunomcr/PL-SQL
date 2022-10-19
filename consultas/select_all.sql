6613bb894aa64b155663b25e941e2e88d564145d
bruno <bruno.mcreal@gmail.com>
2022-10-18 21:38:20 -0300

    Create Dir, Basic select * from all tables

---
+++ consultas/select_all.sql
@@ -0,0 +1,9 @@
+select * from NOTAS_FISCAIS fetch  first 10 rows only;
+
+select * from ITENS_NOTAS_FISCAIS fetch  first 10 rows only;
+
+select * from TABELA_DE_PRODUTOS;
+
+select * from TABELA_DE_CLIENTES;
+
+select * from TABELA_DE_VENDEDORES;
