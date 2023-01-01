ca8f01f9e5efa35494c8c3954feaedbf828022f4
acer@bruno <bruno.mcreal@gmail.com>
2023-01-01 07:39:34 -0300

    change script to run with functions

--- pyOracle/carga_itens_notas_fiscais.py
+++ pyOracle/carga_itens_notas_fiscais.py
@@ -1,34 +1,36 @@
 import pandas as pd
-import db_conexao
 
-dataframe = pd.read_csv(r'C:\Users\bruno\PycharmProjects\PL-SQL\src\carga_itens_notas_fiscais.csv',
-                        delimiter=";", header=None, dtype={1:str})
 
-# Data - Notas Fiscais
-rows = []
-list_itens = []
-for row in range(len(dataframe)):
-    for iten in dataframe.iloc[row]:
-        list_itens.append(iten)
-    list_itens[0] = int(list_itens[0])
-    list_itens[2] = int(list_itens[2])
-    list_itens[3] = float(list_itens[3])
-    tuple_itens = tuple(list_itens) # (103, ' 520380', 29, 12.011)
-    rows.append(tuple_itens)
-    tuple_itens = tuple()
+def load_items_invoce(connection, resposta):
+
+    dataframe = pd.read_csv(fr'C:\Users\bruno\PycharmProjects\PL-SQL\src\carga_itens_notas_fiscais_{resposta}.csv',
+                            delimiter=";", header=None, dtype={1:str})
+
+    # Data - Notas Fiscais
+    rows = []
     list_itens = []
+    for row in range(len(dataframe)):
+        for iten in dataframe.iloc[row]:
+            list_itens.append(iten)
+        list_itens[0] = int(list_itens[0])
+        list_itens[2] = int(list_itens[2])
+        list_itens[3] = float(list_itens[3])
+        tuple_itens = tuple(list_itens) # (103, ' 520380', 29, 12.011)
+        rows.append(tuple_itens)
+        tuple_itens = tuple()
+        list_itens = []
 
-# Cursor
-cursor = db_conexao.connection.cursor()
+    # Cursor
+    cursor = connection.cursor()
 
-# Insert data
-cursor.executemany(
-    "insert into tb_itens_notas_fiscais (NUMERO, CODIGO_DO_PRODUTO, QUANTIDADE, PRECO)"
-    " values(:1, :2, :3, :4)",
-    rows,
-)
+    # Insert data
+    cursor.executemany(
+        "insert into tb_itens_notas_fiscais (NUMERO, CODIGO_DO_PRODUTO, QUANTIDADE, PRECO)"
+        " values(:1, :2, :3, :4)",
+        rows,
+    )
 
-print(cursor.rowcount, "Rows Inserted")
+    print(cursor.rowcount, "Rows Inserted")
 
-# Commit
-db_conexao.connection.commit()
+    # Commit
+    connection.commit()
